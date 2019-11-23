a3ea5047342b9c80e2b5feb51d787f02f2ac33d5
Stefanos Papadam <stefanospapadam@gmail.com>
2019-11-23 10:50:03 +0200

    First attempt,tracking ball and other objects by color

---
+++ ball.py
@@ -0,0 +1,22 @@
+import numpy as np
+import cv2
+
+cap = cv2.VideoCapture('Brooklyn.mp4')
+
+while(cap.isOpened()):
+    ret, frame = cap.read()
+
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #low = np.array([5, 50, 50],np.uint8)
+    #high = np.array([15, 255, 255],np.uint8)
+    low = np.array([11, 71, 71],np.uint8)
+    high = np.array([11, 220, 220],np.uint8)
+    mask = cv2.inRange(hsv,low,high)
+
+    cv2.imshow('black',mask)
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(35) & 0xFF == 27:
+        break
+
+cap.release()
+cv2.destroyAllWindows()
