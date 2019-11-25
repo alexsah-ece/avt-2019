4238a85b61c8d59f2b7530690b85af1713c2220a
Stefanos Papadam <stefanospapadam@gmail.com>
2019-11-25 22:10:34 +0200

    Create trackbar

--- ball.py
+++ ball.py
@@ -1,36 +1,46 @@
 import cv2
 import numpy as np
+def nothing(a):
+    pass
 cap = cv2.VideoCapture('steph.mp4')
+cv2.namedWindow('black')
+img = cv2.imread('screen2.png',1)
+dim = (500, 500)
+img = cv2.resize(img, dim) 
+cv2.createTrackbar('RL','black',0,255,nothing)
+cv2.createTrackbar('GL','black',0,255,nothing)
+cv2.createTrackbar('BL','black',0,255,nothing)
 
-cv2.createTrackbar('R','black',0,255,nothing)
-cv2.createTrackbar('G','black',0,255,nothing)
-cv2.createTrackbar('B','black',0,255,nothing)
-
+cv2.createTrackbar('RH','black',0,255,nothing)
+cv2.createTrackbar('GH','black',0,255,nothing)
+cv2.createTrackbar('BH','black',0,255,nothing)
 
 while(cap.isOpened()):
     ret, frame = cap.read()
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+   
     #low = np.array([5, 50, 50],np.uint8)
     #high = np.array([15, 255, 255],np.uint8)
-    circles = cv2.HoughCircles(gray,cv2.HOUGH_GRADIENT,1.2,400)
-    low = (5, 50, 50)
-    high = (15, 255, 255)
+    
+    rl = cv2.getTrackbarPos('RL','black')
+    gl = cv2.getTrackbarPos('GL','black')
+    bl = cv2.getTrackbarPos('BL','black')
+    
+    rh = cv2.getTrackbarPos('RH','black')
+    gh = cv2.getTrackbarPos('GH','black')
+    bh = cv2.getTrackbarPos('BH','black')
+    
+    low = (rl, gl, bl)
+    high = (rh, gh, bh)
     mask = cv2.inRange(hsv,low,high)
-    if circles is not None:
-        circles = np.round(circles[0,:]).astype("int")
-        for (x, y, r) in circles:
-            if mask[y,x] != 0:
-                cv2.circle(frame, (x, y), r, (0, 255, 0), 4)
+    
                 
-    r = cv2.getTrackbarPos('R','image')
-    g = cv2.getTrackbarPos('G','image')
-    b = cv2.getTrackbarPos('B','image')
 
     cv2.imshow('black',mask)
-    cv2.imshow('frame',frame)
-    if cv2.waitKey(10) & 0xFF == 27:
+    cv2.imshow('image',img)
+    #cv2.imshow('frame',frame)
+    if cv2.waitKey(70) & 0xFF == 27:
         break
 
 cap.release()
