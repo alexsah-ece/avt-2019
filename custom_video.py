0da35f5afdc2893a5bc5f3d8ccabaf6d4c446744
Stathis Dimitriadis <stathisdim@gmail.com>
2019-12-07 17:50:20 +0200

    Improve video quality

--- custom_video.py
+++ custom_video.py
@@ -29,12 +29,12 @@ def hook_handler(d):
 
 # For options check https://github.com/ytdl-org/youtube-dl/blob/3e4cedf9e8cd3157df2457df7274d0c842421945/youtube_dl/YoutubeDL.py#L137-L312
 ydl_opts = {
-    'format': 'best[width<=720]', # We want 720p videos if they are available
+    'format': 'best[width>=720]', # We want 720p videos if they are available
     'logger': SimpleLogger(),
     'progress_hooks': [hook_handler],
     'format': 'mp4',
     'merge-output-format': 'mp4',
-    'writesubtitles': True, 
+    'writesubtitles': False, 
     'outtmpl': "videos/%(title)s.%(ext)s",
     'subtitleslangs': ['el'] # Download greek subtitles
 }
