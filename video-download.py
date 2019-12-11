f9a49ba683c8401304824da636d10ad2ec53d024
Alexandros Sahinis <alexsah97@gmail.com>
2019-12-11 21:53:00 +0200

    Rename file

--- video-download.py
+++
@@ -1,6 +0,0 @@
-from __future__ import unicode_literals
-import youtube_dl
-
-ydl_opts = {}
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download(['https://www.youtube.com/watch?v=ZZOrrqt-Xdo'])
