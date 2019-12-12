a859cb84ad6495f5f23402fcbd4a2c6cadeafd41
Stathis Dimitridiadis <stathisdim@gmail.com>
2019-12-12 23:53:50 +0200

    Create frame extraction script

--- README.md
+++ README.md
@@ -8,6 +8,10 @@ To install the required packages run:
 pip install requirements.txt
 ```
 
+Add ffmpeg with
+```
+sudo apt-get install ffmpeg
+```
 ## Video download & Crop
 
 ### Video Download
@@ -20,16 +24,26 @@ The videos will be downloaded into the `videos` directory.
 
 ### Video Crop
 To crop an already downloaded video you can run:
-```
+```shell
 python crop_video.py {VIDEO_TO_CROP} \
     -o {CROPPED_VIDEO} \
     --start {START_TIME_IN_SECONDS} \
     --end 20 {END_TIME_IN_SECONDS}
 ```
 Example - Crop video and retain the part between 5-15 seconds, saving its output to `mydata/cropped.mp4`:
-```
+```shell
 python crop_video.py videos/full_video.mkv \
     -o videos/cropped.mkv \
     --start 5 \
     --end 15
+```
+
+### Frames extraction
+To extract frames from an already downloaded video you can run
+```shell
+    python extract_frames.py {PATH_TO_VIDEO} --start {START_TIME in format hh:mm:ss} --end {END_TIME in format hh:mm:ss}
+```
+Example - Extract frames from 1'25" to 1'49"
+```shell
+python extract_frames.py videos/76ers_vs_nuggets_dec2019.mp4 --start 00:01:25 --end 00:01:49
 ```
--- extract_frames.py
+++ extract_frames.py
@@ -1,14 +1,63 @@
-import ffmpeg
 import sys
-# ffmpeg -i iowa.mp4 -ss 00:09:13.200 -vframes 20 thumb%04d.jpg -hide_banner
+import subprocess
+import os
+from argparse import ArgumentParser
 
-# argv[1] is filename for extraction
-# argv[2] is starting timestamp in format hh:mm:ss.msc 
-# argv[3] is ending timestamp, same format as argv[2]
 
-# ffmpeg -i movie.mp4 -ss 00:00:03 -t 00:00:08 -async 1 -c copy cut.mp4 
+def initiate_extraction():
+    args = get_args()
+    validate_args(args)
+    extract_frames(args)
 
-filename = sys.argv[1]
-start_time = sys.argv[2]
-end_time = sys.argv[3]
 
+def get_args():
+    parser = ArgumentParser()
+    parser.add_argument(
+        "--start",
+        type=str,
+        dest="start",
+        required=True
+    )
+    parser.add_argument(
+        "--end",
+        type=str,
+        dest="end",
+        required=True
+    )
+    parser.add_argument(
+        "input",
+        type=str,
+        default="."
+    )
+    return parser.parse_args()
+
+
+def validate_args(args):
+    # Check if the provided arguments have the correct format
+    time_argument_validation(args.start)
+    time_argument_validation(args.end)
+
+
+def time_argument_validation(time_arg):
+    splitted_arg = time_arg.split(':')
+    if (len(splitted_arg) != 3):
+        raise Exception('--start and --end must follow format hh:mm:ss')
+
+    for arg in splitted_arg:
+        if (len(arg) != 2 or int(arg[0]) >= 6):
+            raise Exception(
+                'each of --start and --end arguments must be in range [00,59]')
+
+
+def extract_frames(args):
+    if not os.path.exists('frames/'):
+        os.mkdir('frames/')
+
+    input_name = args.input.split('/')[-1].split('.')
+    command = f'ffmpeg -i {args.input} -ss {args.start} -to {args.end} ./frames/{input_name[0]}-frame%04d.jpg'
+    command = command.split(' ')
+    subprocess.call(command)
+
+
+if "__name__==__main__":
+    initiate_extraction()
