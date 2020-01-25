80b3e2f76cbb9f31aee9b650ef66eefb5f499dcc
Stefanos Papadam <stefanospapadam@gmail.com>
2020-01-25 21:31:21 +0200

    Fix .gitignore conflicts

--- ball.py
+++ ball.py
@@ -9,14 +9,14 @@ while(cap.isOpened()):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     #low = np.array([5, 50, 50],np.uint8)
     #high = np.array([15, 255, 255],np.uint8)
-    low = np.array([11, 71, 71],np.uint8)
+    low = np.array([10, 70, 70],np.uint8)
     high = np.array([11, 220, 220],np.uint8)
     mask = cv2.inRange(hsv,low,high)
 
     cv2.imshow('black',mask)
     cv2.imshow('frame',frame)
-    if cv2.waitKey(35) & 0xFF == 27:
+    if cv2.waitKey(33) & 0xFF:
         break
-
+    
 cap.release()
 cv2.destroyAllWindows()
